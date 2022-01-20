d1297cc5ee00d845be9ab7820755a7d9534346a8
BuiHuy22yo <BuiHuy22yo@gmail.com>
2022-01-20 15:41:20 +0700

    setup ctwp-helper

--- inc/func/helpers.php
+++ inc/func/helpers.php
@@ -5,3 +5,106 @@
  * Date: 1/20/2022
  * Time: 10:30 AM
  */
+
+if(!function_exists('ctwpGetAllPostsSelect2')):
+    function ctwpGetAllPostsSelect2($post_type='post') {
+        $arr = array();
+        $posts = get_posts(array(
+            'post_type' => $post_type,
+            'posts_per_page' => 20,
+            'order' => 'DESC',
+            'orderby' => 'date',
+        ));
+        if (!empty($posts)) {
+            foreach ($posts as $post) {
+                $arr[$post->ID] = $post->post_title;
+            }
+        }
+        return $arr;
+    }
+endif;
+if(!function_exists('ctwpGetAllCategoriesSelect2')):
+    function ctwpGetAllCategoriesSelect2($taxonomy='category') {
+        $arr = array();
+        $terms = get_terms( array(
+            'taxonomy' => $taxonomy,
+            'hide_empty' => false,
+        ) );
+        if (!empty($terms)) {
+            foreach ($terms as $category) {
+                $arr[$category->term_id] = $category->name;
+            }
+        }
+        return $arr;
+    }
+endif;
+if(!function_exists('ctwpStringLimitWords')):
+    function ctwpStringLimitWords($string, $word_limit) {
+        $words = explode(' ', $string, ($word_limit + 1));
+        if(count($words) > $word_limit)
+            array_pop($words);
+        return implode(' ', $words);
+    }
+endif;
+
+function ctwp_template($settings,$taxonomy,$swiper) {
+    $wc_query = array();
+    $params = array(
+        'posts_per_page' => $settings['limit'],
+        'post_type' => array('product', 'product_variation'),
+    );
+    if (!empty($taxonomy)) {
+        $params['tax_query'] = array(
+            array(
+                'taxonomy' => 'product_cat',
+                'field'    => 'slug',
+                'terms'    => $taxonomy,
+            ),
+        );
+    }
+    $wc_query = new WP_Query($params);
+    ?>
+    <div <?php echo isset($settings['slides_per_view_mobile']) ? 'data-limit-mobile='.$settings['slides_per_view_mobile'].' ' : ''; echo isset($settings['slides_per_view_tablet']) ? 'data-limit-tablet='.$settings['slides_per_view_tablet'] . ' ' : '';echo isset($settings['slides_per_view']) ? 'data-limit-desktop='.$settings['slides_per_view'] . ' ' : '' ?> class="elementor-ctwp_product-tabs-wrapper<?php echo ( $swiper == true ) ? " swiper swiper-container" : ""; ?>">
+        <?php if ($swiper === true): ?>
+        <div class="swiper-wrapper">
+            <?php else: ?>
+            <div class="container">
+                <div class="row">
+                    <?php endif; ?>
+                    <?php if ($wc_query->have_posts()) :
+                        while ($wc_query->have_posts()) :
+                            $wc_query->the_post();
+                            $product = wc_get_product( get_the_ID() );
+                            ?>
+                            <div class="col-product <?php echo $product->get_type(); ?><?php echo ( $swiper === true ) ? ' swiper-slide' : ""; ?>">
+                                <div class="item-product">
+                                    <?php echo '<a href="' . esc_url( get_the_permalink() ) . '" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">'; ?>
+                                    <?php woocommerce_show_product_loop_sale_flash(); ?>
+                                    <?php woocommerce_template_loop_product_thumbnail(); ?>
+                                    <?php echo '<h2 class="' . esc_attr( 'woocommerce-loop-product__title' ) . '">' . get_the_title() . '</h2>'; ?>
+                                    <?php
+                                    if ( wc_review_ratings_enabled() ) {
+                                        echo wc_get_rating_html( $product->get_average_rating() );
+                                    }
+                                    ?>
+                                    <?php woocommerce_template_loop_price(); ?>
+                                    <?php echo '</a>'; ?>
+                                    <?php echo "<div class=primary-button>";woocommerce_template_loop_add_to_cart(array()); echo '</div>' ?>
+                                </div>
+                            </div>
+                        <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    else:  ?>
+                        <p><?php _e( 'No Products' );?></p>
+                    <?php endif; ?>
+                    <?php if ($swiper === true): ?>
+                </div>
+                <div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div>
+                <?php else: ?>
+            </div>
+        </div>
+    <?php endif; ?>
+    </div>
+    <?php
+}
--- main.php
+++ main.php
@@ -9,107 +9,185 @@ Author URI:
 */
 
 if (!defined('ABSPATH')) {
-	return;
+    return;
 }
 
 if (!class_exists('CTWP_Helper')) {
-	class CTWP_Helper
-	{
-		public function __construct()
-		{
-			$this->define();
-			$this->load_library();
-			$this->load_helper();
-			$this->load_custom_page();
+    class CTWP_Helper
+    {
+        public function __construct()
+        {
+            $this->define();
+            $this->load_helper();
+            $this->load_custom_page();
 
-//			require_once CTWP_HELPER_DIR_PATH . 'vendor/autoload.php';
+            add_action('admin_menu', array($this, 'add_menu_page'), 1);
+            add_action('admin_menu', array($this, 'remove_submenu_page'));
 
-			add_action('init', array(__CLASS__, 'load_config'), 2);
-			add_action('admin_menu', array($this, 'add_menu_page'), 1);
-			add_action('admin_menu', array($this, 'remove_submenu_page'));
+            if (is_admin()) {
+                add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
+            }
 
-			if (is_admin()) {
-				add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_scripts'));
-			}
-		}
+            if ($this->ctwp_is_plugin_active('elementor/elementor.php')) {
 
-		// define
-		public function define()
-		{
-			define('CTWP_HELPER_DIR_PATH', plugin_dir_path(__FILE__));
-			define('CTWP_HELPER_DIR_URL', plugin_dir_url(__FILE__));
+                add_action('elementor/frontend/after_register_styles', [$this, 'ctwp_elements_register_styles']);
+                add_action('elementor/frontend/after_register_scripts', [$this, 'ctwp_elements_register_scripts']);
+                add_action('elementor/frontend/after_enqueue_styles', [$this, 'ctwp_elements_enqueue_styles']);
+                add_action('elementor/frontend/after_enqueue_scripts', [$this, 'ctwp_elements_enqueue_scripts']);
+                add_action('elementor/elements/categories_registered', [$this, 'ctwp_add_elementor_widget_categories']);
+                add_action('elementor/widgets/widgets_registered', [$this, 'ctwp_includes_widget_elements']);
+//                add_action('wp_ajax_ctwp_product_tabs_by_taxonomy', array($this, 'ctwp_product_tabs_by_taxonomy'));
+//                add_action('wp_ajax_nopriv_wp_ajax_ctwp_product_tabs_by_taxonomy', array($this, 'ctwp_product_tabs_by_taxonomy'));
+            }
+            $this->ctwp_include_function_plugins();
+        }
 
-			define('CTWP_OPTIONS', '_ctwp_options');
-			define('CTWP_PJ_CUSTOM_USER_OPTIONS', '_ctwp_custom_user_options');
-			define('CTWP_METABOX_BENHNHAN', '_ctwp_benhnhan_options');
-			define('CTWP_METABOX_HOSO', '_ctwp_hoso_options');
-		}
+        // define
+        public function define()
+        {
+            define('CTWP_HELPER_DIR_PATH', plugin_dir_path(__FILE__));
+            define('CTWP_HELPER_DIR_URL', plugin_dir_url(__FILE__));
 
-		// load library.
-		public function load_library()
-		{
-
-		}
-
-		// load config.
-		public static function load_config()
-		{
-
-		}
-
-		// load helper.
-		public function load_helper()
-		{
-			require_once CTWP_HELPER_DIR_PATH . '/inc/func/base.php';
-			require_once CTWP_HELPER_DIR_PATH . '/inc/func/post-type.php';
-			require_once CTWP_HELPER_DIR_PATH . '/inc/func/helpers.php';
-			require_once CTWP_HELPER_DIR_PATH . '/inc/func/hooks.php';
-			require_once CTWP_HELPER_DIR_PATH . '/inc/func/filter.php';
-		}
-
-		public function add_menu_page()
-		{
-			add_menu_page(
-				esc_html__('Quản Lý', 'ctwp-helper'),
-				esc_html__('Quản Lý', 'ctwp-helper'),
-				'edit_posts',
-				'quanly',
-				array($this, 'callback_menu'),
-				'dashicons-hammer'
-			);
-		}
-
-		public function callback_menu()
-		{
-
-		}
-
-		public function remove_submenu_page()
-		{
-			remove_submenu_page('quanly', 'quanly');
-		}
-
-		public function load_custom_page()
-		{
-                require_once CTWP_HELPER_DIR_PATH . '/custom-page/classes/class-database-structure.php';
-				require_once CTWP_HELPER_DIR_PATH . '/custom-page/classes/class-submenu-page.php';
-				require_once CTWP_HELPER_DIR_PATH . '/custom-page/classes/class-hoso.php';
-		}
-
-		public function enqueue_admin_scripts()
-		{
-			wp_enqueue_style('fft_fontawesome-back-end', "https://use.fontawesome.com/releases/v5.12.0/css/all.css");
-			wp_enqueue_style('ctwp_style_hoso', CTWP_HELPER_DIR_URL . 'assets/css/hoso.css', array(), false);
+            define('CTWP_OPTIONS', '_ctwp_options');
+            define('CTWP_PJ_CUSTOM_USER_OPTIONS', '_ctwp_custom_user_options');
+            define('CTWP_METABOX_BENHNHAN', '_ctwp_benhnhan_options');
+            define('CTWP_METABOX_HOSO', '_ctwp_hoso_options');
 
 
-			wp_enqueue_script('hoso-script', CTWP_HELPER_DIR_URL . '/assets/js/hoso.js', array('jquery'), false, true);
-			wp_localize_script('hoso-script', 'ctwp_script', array(
-				'ajax_url'		=> admin_url('admin-ajax.php')
-			));
+            define('CTWP_ELEMENTS_VERSION', '1.0.0' );
+            define('CTWP_ELEMENTS_PATH', plugin_dir_path(__FILE__));
+            define('CTWP_ELEMENTS_NAME', plugin_basename(__FILE__));
+            define('CTWP_ELEMENTS_URL', plugin_dir_url( __FILE__ ));
+        }
+
+        // load helper.
+        public function load_helper()
+        {
+            require_once CTWP_HELPER_DIR_PATH . '/inc/func/base.php';
+            require_once CTWP_HELPER_DIR_PATH . '/inc/func/post-type.php';
+            require_once CTWP_HELPER_DIR_PATH . '/inc/func/helpers.php';
+            require_once CTWP_HELPER_DIR_PATH . '/inc/func/hooks.php';
+            require_once CTWP_HELPER_DIR_PATH . '/inc/func/filter.php';
+        }
+
+        public function add_menu_page()
+        {
+            add_menu_page(
+                esc_html__('Quản Lý', 'ctwp-helper'),
+                esc_html__('Quản Lý', 'ctwp-helper'),
+                'edit_posts',
+                'quanly',
+                array($this, 'callback_menu'),
+                'dashicons-hammer'
+            );
+        }
+
+        public function callback_menu()
+        {
+
+        }
+
+        public function remove_submenu_page()
+        {
+            remove_submenu_page('quanly', 'quanly');
+        }
+
+        public function load_custom_page()
+        {
+            require_once CTWP_HELPER_DIR_PATH . '/custom-page/classes/class-database-structure.php';
+            require_once CTWP_HELPER_DIR_PATH . '/custom-page/classes/class-submenu-page.php';
+            require_once CTWP_HELPER_DIR_PATH . '/custom-page/classes/class-hoso.php';
+        }
+
+        public function enqueue_admin_scripts()
+        {
+            wp_enqueue_style('fft_fontawesome-back-end', "https://use.fontawesome.com/releases/v5.12.0/css/all.css");
+            wp_enqueue_style('ctwp_style_hoso', CTWP_HELPER_DIR_URL . 'assets/css/hoso.css', array(), false);
+
+
+            wp_enqueue_script('hoso-script', CTWP_HELPER_DIR_URL . '/assets/js/hoso.js', array('jquery'), false, true);
+            wp_localize_script('hoso-script', 'ctwp_script', array(
+                'ajax_url' => admin_url('admin-ajax.php')
+            ));
 
 //            wp_enqueue_style('style-admin',CTWP_HELPER_DIR_URL.'assets/css/style-admin.css', false, "1.0.0");
-		}
-	}
+        }
 
-	new CTWP_Helper();
+
+        public function ctwp_include_function_plugins()
+        {
+
+            if (!function_exists('is_plugin_active')) {
+                require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+            }
+
+            // require_once CTWP_ELEMENTS_PATH . 'inc/widgets/class-ctwp-widget-accordion.php';
+            // require_once CTWP_ELEMENTS_PATH . 'inc/widgets/class-ctwp-widget-contact-advanced.php';
+            // require_once CTWP_ELEMENTS_PATH . 'inc/widgets/class-ctwp-widget-recent-blog.php';
+            // require_once CTWP_ELEMENTS_PATH . 'inc/widgets/class-ctwp-widget-social-advanced.php';
+        }
+
+        public function ctwp_includes_widget_elements()
+        {
+            require_once CTWP_ELEMENTS_PATH . 'inc/modules/testimonials-swiper.php';
+        }
+
+        public function ctwp_add_elementor_widget_categories($elements_manager)
+        {
+
+            $elements_manager->add_category(
+                'ctwp-elements',
+                [
+                    'title' => __('Ctwp Elements', 'ctwp-elements'),
+                    'icon' => 'fa fa-plug',
+                ]
+            );
+        }
+
+        public function ctwp_elements_register_styles()
+        {
+            wp_register_style('ctwp-posts-elementor', CTWP_ELEMENTS_URL . 'assets/css/main.css', array(), '1.0.0', 'all');
+        }
+
+        public function ctwp_elements_register_scripts()
+        {
+            wp_register_script('ctwp-posts-elementor', CTWP_ELEMENTS_URL . 'assets/js/main.js', array('jquery', 'swiper'), '1.0.0', 'all');
+        }
+
+        public function ctwp_elements_enqueue_styles()
+        {
+            wp_enqueue_style('ctwp-posts-elementor');
+        }
+
+        public function ctwp_elements_enqueue_scripts()
+        {
+            wp_enqueue_script('ctwp-posts-elementor');
+        }
+
+        public function ctwp_is_plugin_active($plugin)
+        {
+            return in_array($plugin, (array)get_option('active_plugins', array()));
+        }
+
+        public function ctwp_product_tabs_by_taxonomy()
+        {
+            if (!empty($_POST)) {
+                $taxonomy = $_POST['taxonomy'];
+                $postID = $_POST['postID'];
+                $elementID = $_POST['elementID'];
+                $elementType = $_POST['elementType'];
+                $get_settings = new \ThzelGetElementSettings($postID, $elementID, $elementType);
+                $settings = $get_settings->get_settings();
+                if ($elementType == 'ctwp-product-tabs-swiper') {
+                    echo ctwp_template($settings, $taxonomy, true);
+                } else {
+                    echo ctwp_template($settings, $taxonomy, false);
+                }
+
+            }
+            die();
+        }
+    }
+
+    new CTWP_Helper();
 }
